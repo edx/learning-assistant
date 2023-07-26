926b844b15a717dfb233dd7bee89c3a424db6ced
Alie Langston <alangsto@wellesley.edu>
2023-07-26 16:35:50 -0400

    feat: initial files for learning assistant plugin

---
+++ learning_assistant/__init__.py
@@ -0,0 +1,7 @@
+"""
+Plugin for a learning assistant backend, intended for use within edx-platform.
+"""
+
+__version__ = '0.1.0'
+
+default_app_config = 'learning_assistant.apps.LearningAssistantConfig'  # pylint: disable=invalid-name
---
+++ learning_assistant/apps.py
@@ -0,0 +1,13 @@
+"""
+learning_assistant Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class LearningAssistantConfig(AppConfig):
+    """
+    Configuration for the learning_assistant Django application.
+    """
+
+    name = 'learning_assistant'
