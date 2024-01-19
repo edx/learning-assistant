9292fee9a0ee15c504890e6a722c6ad3289ba87f
Michael Roytman <mroytman@2u.com>
2024-01-19 09:44:23 -0500

    feat: remove references to the CoursePrompt model in preparation for its removal (#53)
    This commit removes references to the CoursePrompt model. Because we no longer have plans to store system prompts in a model, we plan to remove this model and drop the associated table. In preparation for this step, we need to remove references to the model.

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,11 +13,8 @@ from learning_assistant.api import (
     _get_children_contents,
     _leaf_filter,
     get_block_content,
-    get_deserialized_prompt_content_by_course_id,
-    get_setup_messages,
     render_prompt_template,
 )
-from learning_assistant.models import CoursePrompt
 
 fake_transcript = 'This is the text version from the transcript'
 
@@ -51,39 +48,6 @@ class FakeBlock:
 
     def get_children(self):
         return self.children
-
-
-class LearningAssistantAPITests(TestCase):
-    """
-    Test suite for the api module
-    """
-
-    def setUp(self):
-        self.course_id = 'course-v1:edx+test+23'
-        self.prompt = ["This is a Prompt", "This is another Prompt"]
-        self.course_prompt = CoursePrompt.objects.create(
-            course_id=self.course_id,
-            json_prompt_content=self.prompt,
-        )
-        return super().setUp()
-
-    def test_get_deserialized_prompt_valid_course_id(self):
-        prompt_content = get_deserialized_prompt_content_by_course_id(self.course_id)
-        expected_content = self.prompt
-        self.assertEqual(prompt_content, expected_content)
-
-    def test_get_deserialized_prompt_invalid_course_id(self):
-        prompt_content = get_deserialized_prompt_content_by_course_id('course-v1:edx+fake+19')
-        self.assertIsNone(prompt_content)
-
-    def test_get_setup_messages(self):
-        setup_messages = get_setup_messages(self.course_id)
-        expected_messages = [{'role': 'system', 'content': x} for x in self.prompt]
-        self.assertEqual(setup_messages, expected_messages)
-
-    def test_get_setup_messages_invalid_course_id(self):
-        setup_messages = get_setup_messages('course-v1:edx+fake+19')
-        self.assertIsNone(setup_messages)
 
 
 @ddt.ddt
