92b820c72d9d5fa982c297e6dbe77fd4870c6614
Michael Roytman <mroytman@2u.com>
2024-01-23 12:02:56 -0500

    feat!: remove and drop the CoursePrompt model (#54)
    This commit removes and drops the CoursePrompt model. Because we no longer have plans to store system prompts in a model, we have removed and dropped this model.
    
    BREAKING CHANGE:
    
    This commit removes the CoursePrompt model, which means that this model will no longer be supported for specifying the system prompt to use for a course. In lieu of the CoursePrompt model, please use the LEARNING_ASSISTANT_PROMPT_TEMPLATE Django setting to store a system prompt as a Jinja2 query template. Please view the associated architecture decision record (ADR), "2. System Prompt Design Changes", here for more details: https://github.com/edx/learning-assistant/blob/main/docs/decisions/0002-system-prompt-design-changes.rst.

--- learning_assistant/__init__.py
+++ learning_assistant/__init__.py
@@ -2,6 +2,6 @@
 Plugin for a learning assistant backend, intended for use within edx-platform.
 """
 
-__version__ = '2.0.3'
+__version__ = '3.0.0'
 
 default_app_config = 'learning_assistant.apps.LearningAssistantConfig'  # pylint: disable=invalid-name
