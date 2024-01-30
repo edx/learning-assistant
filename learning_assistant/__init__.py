360dc7e80257f478c5b31ed67eaf180c7526d6d7
Alie Langston <alangsto@wellesley.edu>
2024-01-30 15:51:37 -0500

    feat: update release version

--- learning_assistant/__init__.py
+++ learning_assistant/__init__.py
@@ -2,6 +2,6 @@
 Plugin for a learning assistant backend, intended for use within edx-platform.
 """
 
-__version__ = '3.2.0'
+__version__ = '3.3.0'
 
 default_app_config = 'learning_assistant.apps.LearningAssistantConfig'  # pylint: disable=invalid-name
