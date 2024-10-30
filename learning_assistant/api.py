2b27837cc8a3f4ae3d2fc79179cee1d95cb1a211
ilee2u <ilee@2u.com>
2024-10-30 15:56:39 -0400

    chore: LINT

--- learning_assistant/api.py
+++ learning_assistant/api.py
@@ -195,7 +195,6 @@ def get_message_history(course_id, user, message_count):
 
     Returns a number of messages equal to the message_count value.
     """
-
     # If the received message count exceeds the number of messages present, we return all the messages queried.
     actual_message_count = LearningAssistantMessage.objects.count()
     if message_count > actual_message_count:
