7b8425d2a9ee033da96279681730285e85ed94a4
Marcos <mrigoli@2u.com>
2024-11-06 13:03:10 -0300

    fix: Fixed package version

--- learning_assistant/__init__.py
+++ learning_assistant/__init__.py
@@ -2,6 +2,6 @@
 Plugin for a learning assistant backend, intended for use within edx-platform.
 """
 
-__version__ = '4.4.1'
+__version__ = '4.4.3'
 
 default_app_config = 'learning_assistant.apps.LearningAssistantConfig'  # pylint: disable=invalid-name
