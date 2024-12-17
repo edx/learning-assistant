cdaa9b95df8c6d88f9050aab6f8f65970e1ea078
ilee2u <ilee@2u.com>
2024-12-17 14:53:18 -0500

    feat: add admin table for audit trial

--- learning_assistant/admin.py
+++ learning_assistant/admin.py
@@ -1,9 +1,12 @@
 """
 Django Admin pages.
 """
+from datetime import timedelta
+
+from django.conf import settings
 from django.contrib import admin
 
-from learning_assistant.models import LearningAssistantCourseEnabled
+from learning_assistant.models import LearningAssistantAuditTrial, LearningAssistantCourseEnabled
 
 
 @admin.register(LearningAssistantCourseEnabled)
@@ -11,3 +14,22 @@ class LearningAssistantCourseEnabledAdmin(admin.ModelAdmin):
     """
     Admin panel for the LearningAssistantCourseEnabled model.
     """
+@admin.register(LearningAssistantAuditTrial)
+class LearningAssistantAuditTrialAdmin(admin.ModelAdmin):
+    """
+    Admin panel for the LearningAssistantAuditTrial model.
+
+    NOTE: When viewed in admin, these datetimes are in UTC.
+    Please take this into account when reading/modifying entries.
+    """
+    @admin.display(description="Expiration Date")
+    def expiration_date(obj):
+        return obj.start_date + timedelta(days=settings.LEARNING_ASSISTANT_AUDIT_TRIAL_LENGTH_DAYS)
+
+    list_display = ('user', 'start_date', expiration_date)
+    raw_id_fields = ('user',)
+    search_fields = ('user__username',)
+
+    class Meta:
+        model = LearningAssistantAuditTrial
+        fields = ('user', 'start_date',)
