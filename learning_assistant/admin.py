555f4beab7cf4f05de3150adb493ae38b75130c9
Isaac Lee <124631592+ilee2u@users.noreply.github.com>
2024-12-17 15:43:20 -0500

    Ilee2u/audit trial admin (#145)
    * feat: add admin table for audit trial
    
    * docs: add ENABLE_XPERT_AUDIT instructions
    
    * chore: lint
    
    * docs: update version + changelog

--- learning_assistant/admin.py
+++ learning_assistant/admin.py
@@ -1,9 +1,12 @@
 """
 Django Admin pages.
 """
+from datetime import timedelta
+
+from django.conf import settings
 from django.contrib import admin
 
-from learning_assistant.models import LearningAssistantCourseEnabled
+from learning_assistant.models import LearningAssistantAuditTrial, LearningAssistantCourseEnabled
 
 
 @admin.register(LearningAssistantCourseEnabled)
@@ -11,3 +14,32 @@ class LearningAssistantCourseEnabledAdmin(admin.ModelAdmin):
     """
     Admin panel for the LearningAssistantCourseEnabled model.
     """
+
+
+@admin.register(LearningAssistantAuditTrial)
+class LearningAssistantAuditTrialAdmin(admin.ModelAdmin):
+    """
+    Admin panel for the LearningAssistantAuditTrial model.
+
+    NOTE: When viewed in admin, these datetimes are in UTC.
+    Please take this into account when reading/modifying entries.
+    """
+
+    @admin.display(description="Expiration Date")
+    def expiration_date(self, obj):
+        """
+        Generate the expiration date for the LearningAssistantAuditTrial based on the start_date.
+        """
+        return obj.start_date + timedelta(days=settings.LEARNING_ASSISTANT_AUDIT_TRIAL_LENGTH_DAYS)
+
+    list_display = ('user', 'start_date', expiration_date)
+    raw_id_fields = ('user',)
+    search_fields = ('user__username',)
+
+    class Meta:
+        """
+        Meta for the LearningAssistantAuditTrial admin panel.
+        """
+
+        model = LearningAssistantAuditTrial
+        fields = ('user', 'start_date',)
