98636d7fbfbcf9d5b23a300dff32853131367b85
Marcos <mrigoli@2u.com>
2025-02-11 09:35:40 -0300

    chore: Removed unused chat enabled and chat history endpoints

--- learning_assistant/urls.py
+++ learning_assistant/urls.py
@@ -7,8 +7,6 @@ from learning_assistant.constants import COURSE_ID_PATTERN
 from learning_assistant.views import (
     CourseChatView,
     LearningAssistantChatSummaryView,
-    LearningAssistantEnabledView,
-    LearningAssistantMessageHistoryView,
 )
 
 app_name = 'learning_assistant'
@@ -18,16 +16,6 @@ urlpatterns = [
         fr'learning_assistant/v1/course_id/{COURSE_ID_PATTERN}$',
         CourseChatView.as_view(),
         name='chat'
-    ),
-    re_path(
-        fr'learning_assistant/v1/course_id/{COURSE_ID_PATTERN}/enabled',
-        LearningAssistantEnabledView.as_view(),
-        name='enabled',
-    ),
-    re_path(
-        fr'learning_assistant/v1/course_id/{COURSE_ID_PATTERN}/history',
-        LearningAssistantMessageHistoryView.as_view(),
-        name='message-history',
     ),
     re_path(
         fr'learning_assistant/v1/course_id/{COURSE_ID_PATTERN}/chat-summary',
