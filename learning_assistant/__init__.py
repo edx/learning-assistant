2c4f75258e28045422eb93f484f98a018d3647c6
Maniraja Raman <mraman-apphelix@2u.com>
2025-07-18 14:49:05 +0000

    chore: Update version to 4.10.7 and document changes in CHANGELOG

--- learning_assistant/__init__.py
+++ learning_assistant/__init__.py
@@ -2,6 +2,6 @@
 Plugin for a learning assistant backend, intended for use within edx-platform.
 """
 
-__version__ = '4.10.6'
+__version__ = '4.10.7'
 
 default_app_config = 'learning_assistant.apps.LearningAssistantConfig'  # pylint: disable=invalid-name
