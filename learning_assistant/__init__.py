0ecf72aca34ca7cde95249a14315f61cab291d2a
Akanshu Aich <aaich-apphelix@2u.com>
2026-08-05 10:41:41 +0000

    fix: Added Django 5.2 support in tox and CI

--- learning_assistant/__init__.py
+++ learning_assistant/__init__.py
@@ -2,6 +2,6 @@
 Plugin for a learning assistant backend, intended for use within edx-platform.
 """
 
-__version__ = '4.11.5'
+__version__ = '4.11.6'
 
 default_app_config = 'learning_assistant.apps.LearningAssistantConfig'  # pylint: disable=invalid-name
